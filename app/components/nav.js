993f9a7d41bc5c0ad8adc345fb87894db1dcb932
Cal <calpage18@gmail.com>
2016-04-15 00:56:17 +0100

    Added mobile size menu

--- app/components/nav.js
+++ app/components/nav.js
@@ -7,6 +7,8 @@ export default React.createClass({
     return(
       <header className="header">
         <span className="logo">Wallet</span>
+
+        {/* Display as default. On resize, display this one above mobile*/}
         <nav className="nav">
           <ul className="nav-list">
             <li className="nav-list-item">Home</li>
@@ -14,6 +16,15 @@ export default React.createClass({
             <li className="nav-list-item">View Source</li>
           </ul>
         </nav>
+
+        {/* On resize, display this one at mobile*/}
+        {/*<nav className="nav">
+          <ul className="nav-list">
+            <li className="nav-list-item"><i className="fa fa-home" aria-hidden="true"></i></li>
+            <li className="nav-list-item"><i className="fa fa-refresh" aria-hidden="true"></i></li>
+            <li className="nav-list-item"><i className="fa fa-github" aria-hidden="true"></i></li>
+          </ul>
+        </nav>*/}
       </header>
     )
   }
